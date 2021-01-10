fe9ab0057ebdfd79ac617d7a81dc7d97565523ef
aragrevo <eduver_san@hotmail.com>
2021-01-10 18:25:47 -0500

    whatsapp button

--- src/components/FloatButton/FloatButton.jsx
+++ src/components/FloatButton/FloatButton.jsx
@@ -7,8 +7,8 @@ const FloatButton = () => {
       href='https://api.whatsapp.com/send?phone=573044580373&text=Hola%20Arkon%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n.'
       className='float'
       target='_blank'
+      rel='noopener noreferrer'
     >
-      {/* <i class='fa fa-whatsapp my-float'></i> */}
       <img
         className='my-float'
         src='https://cdn.svgporn.com/logos/whatsapp.svg'
